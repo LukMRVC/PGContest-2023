89d894359bf586063af0a631969ba812bceda8a2
Lukas Moravec <lukas.moravec21@seznam.cz>
2022-11-22 19:33:39 +0100

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-use std::hash::Hash;
+use std::env;
+use std::fs::File;
 use std::io::{prelude::*, stdin, stdout, BufReader};
-use std::ptr::swap;
 
 fn ukkonen(s1: &[u8], s2: &[u8], threshold: usize, record_id: usize) -> usize {
     let mut s1len = s1.len();
     let mut s2len = s2.len();
 
     // if cfg!(debug_assertions) {
-    //     print!("S1:{}\tS2:{}",
-    //              std::str::from_utf8(s1).unwrap(),
-    //              std::str::from_utf8(s2).unwrap(),
-    //         )
+    //     println!(
+    //         "S1:{}\tS2:{}",
+    //         std::str::from_utf8(s1).unwrap(),
+    //         std::str::from_utf8(s2).unwrap(),
+    //     )
     // }
 
     if s1len.abs_diff(s2len) > threshold {
-        return threshold;
+        return 0;
     }
 
     // make sure that |s1| is smaller or equal to s2
@@ -25,7 +26,7 @@ fn ukkonen(s1: &[u8], s2: &[u8], threshold: usize, record_id: usize) -> usize {
     }
 
     if s1len == 0 {
-        return std::cmp::min(s2len, threshold); // if s2len < threshold { record_id } else { 0 };
+        return if s2len < threshold { record_id } else { 0 };
     }
 
     // now prefix trimming
@@ -41,18 +42,19 @@ fn ukkonen(s1: &[u8], s2: &[u8], threshold: usize, record_id: usize) -> usize {
     s2len -= t_start;
 
     if s1len == 0 {
-        return std::cmp::min(s2len, threshold); // if s2len < threshold { record_id } else { 0 };
+        return if s2len < threshold { record_id } else { 0 };
     }
 
-    let threshold = std::cmp::min(s2len, threshold);
+    let new_threshold = std::cmp::min(s2len, threshold);
     let diff_len = s2len - s1len;
 
-    if threshold < diff_len {
-        return threshold;
+    if new_threshold < diff_len {
+        return 0;
     }
 
     // initialize zero_k
-    let zero_k: i32 = ((std::cmp::min(s1len, threshold) >> 1) + 2) as i32;
+    let zero_k: i32 = ((std::cmp::min(s1len, new_threshold) >> 1) + 2) as i32;
+
     let arr_len = diff_len + (zero_k as usize) * 2 + 2;
     let mut current_row = vec![-1i32; arr_len];
     let mut next_row = vec![-1i32; arr_len];
@@ -63,10 +65,6 @@ fn ukkonen(s1: &[u8], s2: &[u8], threshold: usize, record_id: usize) -> usize {
     return loop {
         i += 1;
         std::mem::swap(&mut next_row, &mut current_row);
-        // unsafe {
-        //     swap(&mut next_row, &mut current_row);
-        // }
-        // let (mut current_row, mut next_row) = (next_row, current_row);
 
         let start: i32;
         let mut next_cell: i32;
@@ -115,136 +113,48 @@ fn ukkonen(s1: &[u8], s2: &[u8], threshold: usize, record_id: usize) -> usize {
             row_index += 1;
         }
 
-
-        if !(next_row[condition_row as usize] < (s1len as i32) && i <= (threshold as i32)) {
-            break i as usize - 1;
+        if !(next_row[condition_row as usize] < (s1len as i32) && i <= (new_threshold as i32)) {
+            break if i as usize - 1 <= (threshold - 1) {
+                record_id
+            } else {
+                0
+            };
         }
     };
-
-    // if cfg!(debug_assertions) {
-    //     println!("\t{}", i - 1);
-    // }
-
-    return i as usize - 1;
-    if i - 1 <= (threshold as i32) {
-        record_id
-    } else {
-        0
-    }
-}
-
-fn lve(
-    data: &[Vec<u8>],
-    query_word: &str,
-    threshold: usize,
-    m: &mut [usize],
-    msize: usize,
-) -> usize {
-    let mut sum = 0usize;
-    let qwlen = query_word.len() + 1;
-    let qw = query_word.as_bytes();
-
-    for (id, word) in data.iter().enumerate() {
-        let mut min = 0usize;
-        let wlen = word.len() + 1;
-        // let mut begin = 0usize;
-        // let mut begin_value = 0usize;
-        // let mut end = threshold;
-        // let mut new_max: bool = false;
-
-        if threshold == 0 {
-            if word == qw {
-                sum += id + 1;
-                continue;
-            }
-        }
-        let start_idx = 1;
-        for i in 1..wlen {
-            // if begin > start_idx {
-            //     start_idx = begin;
-            // }
-            // new_max = false;
-            // begin_value = m[(i - 1) * msize + begin];
-            for j in start_idx..qwlen {
-                let insertion = m[(i - 1) * msize + j] + 1;
-                let deletion = m[i * msize + (j - 1)] + 1;
-                let substitution =
-                    m[(i - 1) * msize + (j - 1)] + ((word[i - 1] != qw[j - 1]) as usize);
-                min = std::cmp::min(substitution, std::cmp::min(insertion, deletion));
-                m[i * msize + j] = min;
-            }
-            let rowmin = m[(i * msize)..(i * msize + qwlen)].iter().min().unwrap();
-            if *rowmin > threshold {
-                break;
-            }
-        }
-
-        if min <= threshold {
-            // println!("LVE: {} {} = {}", query_word, sword, min);
-            sum += id + 1;
-        }
-
-        // println!("QW: {}\t{} = {}", query_word, sword, min);
-        // for i in 0..wlen {
-        //     for j in 0..qwlen {
-        //         print!("{} ", m[i * msize + j])
-        //     }
-        //     println!(" ")
-        // }
-        // println!("-------------------------------")
-    }
-
-    return sum;
 }
 
 fn read<R: Read>(reader: &mut BufReader<R>) {
     let mut srchdata: Vec<Vec<u8>> = Vec::<Vec<u8>>::with_capacity(8196usize);
+    // let mut querydata: Vec<(String, usize)> = Vec::<Vec<(String, usize)>>::with_capacity(8196);
     let mut line = String::with_capacity(128);
     let srch_line = "[SEARCH]";
-    let mut longest_word: usize = 0;
 
     // let mut srchquery: Vec<Vec<u8>> = Vec::<Vec<u8>>::with_capacity(8196usize);
 
     // read database words
-    let mut line_len: usize;
     while let Ok(bytes_read) = reader.read_line(&mut line) {
         if bytes_read == 0 {
             break;
         }
         // remove newline
         line.pop();
-        line_len = line.len();
         if srch_line.eq_ignore_ascii_case(&line) {
             break;
-        }
-
-        if line_len > longest_word {
-            longest_word = line_len;
         }
 
         srchdata.push(line.clone().into_bytes());
         line.clear();
     }
-    // for the zeroth elem
-    longest_word += 1;
-    // let mut matrix = vec![0usize; longest_word * longest_word];
-    // initialize first row
-    // for i in 0..longest_word {
-    //     matrix[i] = i;
-    // }
-    // // initialize first column
-    // for i in 0..longest_word {
-    //     matrix[i * longest_word] = i;
-    // }
 
     line.clear();
     // read query words
     let mut sum: usize = 0;
+    // println!("{}", longest_word);
+
     while let Ok(bytes_read) = reader.read_line(&mut line) {
         if bytes_read == 0 {
             break;
         }
-        // remove newline
         line.pop();
 
         let Some((query_word, t)) = line.split_once(',') else {
@@ -256,22 +166,11 @@ fn read<R: Read>(reader: &mut BufReader<R>) {
         let qwbytes = query_word.as_bytes();
         for (id, word) in srchdata.iter().enumerate() {
             if (word.len() > qwlen) {
-                let tres = ukkonen(qwbytes, word, t + 1, id + 1);
-                // println!("{}\t{}\t{}", std::str::from_utf8(word).unwrap(), query_word, tres);
-                if tres <= t {
-                    sum += id + 1;
-                }
+                sum += ukkonen(qwbytes, word, t + 1, id + 1);
             } else {
-                let tres =  ukkonen(word, qwbytes, t + 1, id + 1);
-                // println!("{}\t{}\t{}", std::str::from_utf8(word).unwrap(), query_word, tres);
-                if tres <= t {
-                    sum += id + 1;
-                }
+                sum += ukkonen(word, qwbytes, t + 1, id + 1);
             }
         }
-        // sum += lve(&srchdata, word, t, &mut matrix, longest_word);
-
-        // srchquery.push(line.clone().into_bytes());
         line.clear();
     }
 
@@ -280,15 +179,15 @@ fn read<R: Read>(reader: &mut BufReader<R>) {
 }
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let filename: &str;
-    // if args.len() > 1 {
-    //     filename = &args[1];
-    //     let file = File::open(filename).unwrap();
-    //     let mut reader = BufReader::new(file);
-    //     read(&mut reader);
-    // } else {
-    let mut reader = BufReader::new(stdin().lock());
-    read(&mut reader);
-    // }
+    let args: Vec<String> = env::args().collect();
+    let filename: &str;
+    if args.len() > 1 {
+        filename = &args[1];
+        let file = File::open(filename).unwrap();
+        let mut reader = BufReader::new(file);
+        read(&mut reader);
+    } else {
+        let mut reader = BufReader::new(stdin().lock());
+        read(&mut reader);
+    }
 }
