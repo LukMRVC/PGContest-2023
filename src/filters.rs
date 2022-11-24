fc96de69afe405cf999b2d400780a1b34659d5cc
Lukas Moravec <lukas.moravec21@seznam.cz>
2022-11-24 23:36:58 +0100

    Better QGrams

--- src/filters.rs
+++ src/filters.rs
@@ -1,50 +1,108 @@
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone)]
-pub struct Qgram<'a> {
-    profile: HashMap<&'a [u8], usize>,
+pub struct Qgram {
+    // profile: Vec<&'a [u8]>,
+    ranking_profile: Vec<usize>,
 }
 
-impl<'a> Qgram<'a> {
-    pub fn new(s: &'a [u8], q: usize) -> Self {
-        let mut prof = HashMap::<&'a [u8], usize>::new();
-        let end = s.len() - q + 1;
-        for i in 0..end {
-            let slice = &s[i..(i + q)];
-            if let Some(val) = prof.get_mut(slice) {
-                *val += 1;
-            } else {
-                prof.insert(slice, 1);
-            }
+impl Qgram {
+    // SIZE of Q-gram
+    const Q: usize = 2;
+    // size of the alphabet
+    const SIGMA: usize = 26;
+    // my ASCII alphabet translations
+    const TRANSLATE_MAP: [usize; 256] = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 16
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 32
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 48
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 64
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, // 80
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0, 0, 0, 0, 0, 0, // 96
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 112
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 128
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 144
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 160
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 176
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 192
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 208
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 224
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 240
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 256
+    ];
+
+    const PROFILE_LEN: usize = Self::SIGMA * Self::SIGMA;
+
+    pub fn new(s: &[u8]) -> Self {
+        let mut ranking_profile = vec![0; Self::PROFILE_LEN];
+        let sdist = s.len() - Self::Q + 1;
+        let mut init_rank = Self::rank2(&s[0..Self::Q]);
+        ranking_profile[init_rank] += 1;
+        for i in 1..sdist {
+            let r = (init_rank - Self::TRANSLATE_MAP[s[(i - 1)] as usize] * Self::SIGMA)
+                * Self::SIGMA
+                + Self::TRANSLATE_MAP[s[i + Self::Q - 1] as usize];
+            ranking_profile[r] += 1;
+            init_rank = r;
         }
 
-        Qgram { profile: prof }
+        Qgram { ranking_profile }
     }
 
-    pub fn dist(&self, other: &Qgram, t: usize) -> usize {
-        let mut union = HashSet::<&'a [u8]>::new();
-        union.extend(self.profile.keys());
-        union.extend(other.profile.keys());
-
-        let mut agg = 0;
-        for k in union.into_iter() {
-            let (mut v0, mut v1) = (0, 0);
-            if let Some(val) = self.profile.get(k) {
-                v0 += val;
-            }
-            if let Some(val) = other.profile.get(k) {
-                v1 += val;
-            }
-            agg += v0.abs_diff(v1);
-            if agg > 2 * t {
-                return agg;
-            }
+    pub fn dist(q1: &Qgram, q2: &Qgram) -> usize {
+        let mut result = vec![0; Self::PROFILE_LEN];
+        // q1.ranking_profile
+        //     .iter()
+        //     .zip(q2.ranking_profile.iter())
+        //     .fold(0, |accum, (d1, d2)| accum + d1.abs_diff(*d2))
+        for i in (0..(Self::PROFILE_LEN)).step_by(4) {
+            result[i] = q1.ranking_profile[i].abs_diff(q2.ranking_profile[i]);
+            result[i + 1] = q1.ranking_profile[i + 1].abs_diff(q2.ranking_profile[i + 1]);
+            result[i + 2] = q1.ranking_profile[i + 2].abs_diff(q2.ranking_profile[i + 2]);
+            result[i + 3] = q1.ranking_profile[i + 3].abs_diff(q2.ranking_profile[i + 3]);
         }
 
-        agg
+        result.into_iter().sum()
     }
 
-    pub fn Dist(q1: &Qgram, q2: &Qgram) -> usize {
-        q1.dist(q2, 0)
+    #[inline(always)]
+    fn rank2(slice: &[u8]) -> usize {
+        // for Q = 2;
+        Self::TRANSLATE_MAP[slice[0] as usize] * Self::SIGMA
+            + Self::TRANSLATE_MAP[slice[1] as usize]
     }
+
+    #[inline(always)]
+    fn rank3(slice: &[u8]) -> usize {
+        Self::TRANSLATE_MAP[slice[0] as usize] * (Self::SIGMA * Self::SIGMA)
+            + Self::TRANSLATE_MAP[slice[1] as usize] * Self::SIGMA
+            + Self::TRANSLATE_MAP[slice[2] as usize]
+    }
+
+    // pub fn dist(&self, other: &Qgram, t: usize) -> usize {
+    //     let mut union = HashSet::<&'a [u8]>::new();
+    //     union.extend(self.profile.keys());
+    //     union.extend(other.profile.keys());
+
+    //     let mut agg = 0;
+    //     for k in union.into_iter() {
+    //         let (mut v0, mut v1) = (0, 0);
+    //         if let Some(val) = self.profile.get(k) {
+    //             v0 += val;
+    //         }
+    //         if let Some(val) = other.profile.get(k) {
+    //             v1 += val;
+    //         }
+    //         agg += v0.abs_diff(v1);
+    //         if agg > 2 * t {
+    //             return agg;
+    //         }
+    //     }
+
+    //     agg
+    // }
+
+    // pub fn Dist(q1: &Qgram, q2: &Qgram) -> usize {
+    //     q1.dist(q2, 0)
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::io::{prelude::*, stdin, stdout, BufReader};
 use std::time::Instant;
 use ukkonen::ukkonen;
 
+use crate::filters::Qgram;
+
 fn read<R: Read>(reader: &mut BufReader<R>) {
     // let start = Instant::now();
     let mut srchdata: Vec<Vec<u8>> = Vec::<Vec<u8>>::with_capacity(1024 * 1024 * 64);
@@ -50,42 +52,48 @@ fn read<R: Read>(reader: &mut BufReader<R>) {
     // let elapsed = start.elapsed();
     // println!("Reading input took: {} MS", elapsed.as_millis());
 
-    let q = 2;
-    let q2 = 2 * q;
-    // let start = Instant::now();
-    let srchgrams: Vec<filters::Qgram> = srchdata
-        .par_iter()
-        .map(|data| filters::Qgram::new(data, q))
-        .collect();
-    // let elapsed = start.elapsed();
-    // println!("Building Qgrams took: {} MS", elapsed.as_millis());
+    let start = Instant::now();
+    let srchgrams: Vec<Qgram> = srchdata.iter().map(|data| Qgram::new(data)).collect();
+    // println!("Building Qgrams took: {} MS", start.elapsed().as_millis());
 
+    let start = Instant::now();
     let sum: usize = querydata
         .par_iter()
         .map(|(query_word, t)| {
             let mut sum = 0;
             let qwlen = query_word.len();
             let qwbytes = query_word.as_bytes();
-            let query_qgram = filters::Qgram::new(qwbytes, q);
+            let query_qgram = Qgram::new(qwbytes);
+            let t2 = *t * 2;
 
-            srchdata.iter().enumerate().for_each(|(id, word)| {
-                if (query_qgram.dist(&srchgrams[id], *t) / q2) <= *t {
+            srchdata
+                .iter()
+                .enumerate()
+                .filter(|(wid, _)| Qgram::dist(&srchgrams[*wid], &query_qgram) <= t2)
+                .for_each(|(id, word)| {
                     if word.len() > qwlen {
                         sum += ukkonen(qwbytes, word, t + 1, id + 1);
                     } else {
                         sum += ukkonen(word, qwbytes, t + 1, id + 1);
                     }
-                }
-            });
+                });
             sum
         })
         .sum();
-
+    // println!("Querying took: {} MS", start.elapsed().as_millis());
     println!("{}", sum);
     stdout().flush().unwrap();
 }
 
 fn main() {
+    // let s1 = "ACDF".to_owned();
+    // let s2 = "ABCD".to_owned();
+
+    // let q1 = Qgram::new(s1.as_bytes());
+    // let q2 = Qgram::new(s2.as_bytes());
+
+    // println!("{} {} {}", s1, s2, Qgram::dist(&q1, &q2));
+
     let args: Vec<String> = env::args().collect();
     let filename: &str;
     if args.len() > 1 {
